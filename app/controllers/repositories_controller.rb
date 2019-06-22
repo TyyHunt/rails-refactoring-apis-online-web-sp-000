13168c5d72262a78566a4b7540d1e709ef9505e5
Tyler Hunt <tyhunt04@gmail.com>
2019-06-22 23:10:33 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,8 +7,10 @@ class RepositoriesController < ApplicationController
   end
 
   def create
-    github = GithubService.new
-    github.create_repo(params[:name])
-    redirect_to root_path
+    response = Faraday.post 'https://api.github.com/user/repos',
+      {name: params[:name]}.to_json,
+      {'Authorization' => "token #{session[:token]}",
+      'Accept' => 'application/json'}
+      redirect_to '/'
   end
 end
